975350a2f6b3eacaa671464e30da35e90806bafa
Vadym <besarabvadim2@gmail.com>
2022-12-23 21:30:25 +0200

    ContactForm edit

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
+//import { nanoid } from 'nanoid'
 
-export ContactForm = ({ }) => {
-    return ( <label ></label >
-            <input type="tel"
-                name="number"
-                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                title="/*Phone number must be digits and can contain spaces, dashes, parentheses and can start with */+"
-                required
-            />)
+
+export class ContactForm extends Component {
+    
+state = {
+   name: '',
 }
+    
+handleChangeName = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+    }
+    
+handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ name:''});
+  };
+    
+    render() {
+        const { name } = this.state;
+
+        return (<form onSubmit={this.handleSubmit}>
+                  <div className="mb-3">
+                    <label htmlFor="name" className="form-label">
+                    <p>Name</p>
+                    </label>
+                <input
+                       onChange={this.handleChangeName}
+                       type="text"
+                       name="name"
+                       value={name}
+                       pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                       title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                       required
+                     />
+                  </div>
+                     <button className="btn btn-primary" type="submit">Add contact</button>
+                </form>)
+              }
+            };
